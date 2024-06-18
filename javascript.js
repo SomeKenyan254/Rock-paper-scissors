cfbf0155f8a0b0c00fca38939c86e2f52c256660
Arnold Korir <144571592+SomeKenyan254@users.noreply.github.com>
2024-06-18 17:33:36 -0600

    functions to pick computer play and human choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log("Hello")
+let humanScore = 0;
+let computerScore = 0;
+function getComputerChoice(){
+   let random =  Math.floor(Math.random()*3)
+     if (random == '1'){
+        message = 'Rock'}
+    else if(random == '0'){
+        message = 'Paper'}
+   else if (random == '2') {
+    message= 'Scissors'
+   }
+     return message
+}
+function getHumanChoice(){
+    choice = prompt('Type a choice between Rock, Paper and Scissors')
+}
+function round(getComputerChoice,getHumanChoice){ 
+}
+console.log(getHumanChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
